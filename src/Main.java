e8caab846323fec7b9f8066ff5c6d073daa9703f
HelloWorld <andikatirtax@gmail.com>
2023-09-29 22:38:41 +0700

    SYNRGY Challenge_2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
-import java.io.IOException;
-import java.util.Scanner;
+import controller.MenuController;
+import model.AppState;
+import model.Order;
+import model.service.FoodAndBeverage;
+import view.MenuView;
+
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        Command app = new Command();
-        while (app.online()){
-            app.menu(sc.nextLine());
-        }
+    public static void main(String[] args) {
+        MenuView menuView = new MenuView();
+        Order orders = new Order();
+        FoodAndBeverage listMenu = new FoodAndBeverage();
+        AppState state = AppState.STANDBY;
+        MenuController controller = new MenuController(listMenu, orders, menuView, state);
+        controller.takeOrder();
     }
 }
--- src/model/AppState.java
+++ src/model/AppState.java
@@ -1,4 +1,5 @@
+package model;
+
 public enum AppState {
     STANDBY, EDIT, FINALIZATION, QTY_INCREASE, QTY_EDIT, EXIT;
 }
-
